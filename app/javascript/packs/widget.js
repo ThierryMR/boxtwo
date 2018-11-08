42db0e5a1eb59710556c66b3ea47601d438c29d7
Henrique Aguilar <henriaguilar@gmail.com>
2018-11-08 14:36:28 -0200

    widget working and changing period

--- app/javascript/packs/widget.js
+++ app/javascript/packs/widget.js
@@ -1,8 +1,34 @@
-function action(currencies) {
-  baseUrl = "https://widgets.cryptocompare.com/";
-  var scripts = document.getElementsByTagName("script");
-  var period = document.getElementById("period");
-  console.log(period.value);
+const widgetCards = document.getElementById("widget-cards")
+const currencies = JSON.parse(widgetCards.dataset.currencies);
+var period = document.getElementById("period");
+console.log(`period fora do event listener loop: ${period.value}`);
+// period.addEventListener('change', action(currencies));
+
+baseUrl = "https://widgets.cryptocompare.com/";
+var scripts = document.getElementsByTagName("script");
+var period = document.getElementById("period");
+console.log(`period dentro do event listener do loop: ${period.value}`);
+
+currencies.forEach((currency) => {
+  var embedder = scripts[currency.symbol];
+  (function (){
+    var appName = encodeURIComponent(window.location.hostname);
+    if(appName==""){appName="local";}
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.async = true;
+    var theUrl = baseUrl+`serve/v2/coin/chart?fsym=${currency.symbol}&tsym=BRL&period=${period.value}`;
+    s.src = theUrl + ( theUrl.indexOf("?") >= 0 ? "&" : "?") + "app=" + appName;
+    embedder.parentNode.appendChild(s);
+  })();
+});
+
+period.addEventListener('change', function() {
+  document.querySelectorAll('.ccc-widget').forEach((widget) => {
+    widget.parentNode.removeChild(widget);
+  });
+  console.log(`period dentro do event listener do loop: ${period.value}`);
+
   currencies.forEach((currency) => {
     var embedder = scripts[currency.symbol];
     (function (){
@@ -16,22 +42,4 @@ function action(currencies) {
       embedder.parentNode.appendChild(s);
     })();
   });
-}
-
-// var scripts = document.getElementsByTagName("script");
-const widgetCards = document.getElementById("widget-cards")
-const currencies = JSON.parse(widgetCards.dataset.currencies);
-// var embedder = scripts[ scripts.length - 1 ];
-var period = document.getElementById("period");
-// newElement = document.createElement("div");
-// newElement.id = currency.symbol;
-// newElement.class = "col-xs-6 col-sm-4 col-md-3";
-console.log(period);
-
-// action(currencies);
-period.addEventListener('change', action(currencies));
-
-
-
-
-// widgetCards.appendChild(newElement);
+});

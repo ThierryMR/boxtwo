d90209c533a060313d5f8b6f1bac3ba4bcc02256
Davis JR <davisjr2000@hotmail.com>
2018-11-08 16:55:46 -0200

    bugged

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,22 @@
 class TransactionsController < ApplicationController
+  def index
+    @transactions = policy_scope(Transaction)
+  end
+
+  def new
+    @transaction = Transaction.new
+    authorize @transaction
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+    authorize @transaction
+    @transaction.save
+    redirect_to transactions_path
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:status, :user_id, :offer_id)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
     resources :offers, only: [:create]
   end
 
-
+  resources :transactions, only: [:index, :create]
   resources :currencies, only: [:index, :show] #totally independent
   resources :offers, only: [:show, :index] do
-    resources :trades, only: [:create]
+    resources :transactions, only: [:new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

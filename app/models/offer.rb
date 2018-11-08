f036f11f44fb14b86b6b5594bbf4e0d93f243998
Thierry Silvagnoli <thierry.martins.r.s@gmail.com>
2018-11-08 15:05:52 -0300

    Uniqueness on currency id on wallet

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,6 @@
 class Offer < ApplicationRecord
   belongs_to :wallet
   has_one :trade, :class_name => "Transaction", foreign_key: "trader_id"
+  # scope :offers_by_currency, -> (currency_name) { where wallet: wallet_id }
 end
+# offers.wallet.currency.name

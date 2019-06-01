85f03b23aacca30c6caff4ddfbd9a6f4bd09ce42
Thierry Silvagnoli <thierry.martins.r.s@gmail.com>
2019-05-31 22:38:29 -0300

    Muito wierd meus commentarios, I think I will remeber what I just typed, I am now getting my bus to BH, vou comecar a armenar meus pensamentos aqui, mesmo sendo um pouco paranoico e desconfiando que tem some entity manipulando alguns comentarios aqui, sou thierry martins ribeiro sil... e gostaria de saber se por um acaso isso eh uma lance de familia... e tentar lembrar com mais accuraccy as coisas que faco , espero ser um bom padrinho

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+# Ao fazer a importacao tente conseguir entender as variacoes do dia low high
+# COm a media da variacao por dia, media do preco no ultimos 7 dias <<< (criando a logica para trades mais curtos)
+# Lembrar de como e um bom ter  um administrador analisando os models
